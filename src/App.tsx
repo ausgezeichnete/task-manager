1487561a723b4af28cd3da056b0c9f44c083cf03
Maha F. <ausgezeichnete@gmail.com>
2026-05-09 12:56:26 +0200

    initial Vite React Typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-8xl font-bold text-blue-600">Task Manager</h1>
+    </div>
+  );
+}
+
+export default App;
